64aee47d118c8a4563344923e125557629ac6c5c
Angela Conde <56882332+angelaconde@users.noreply.github.com>
2021-01-23 14:19:52 +0100

    Created Category model and controller

---
+++ app/Models/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+        /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'categorias';
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+    protected $fillable = [
+        'id',
+        'nombre',
+        'descripcion',
+        'anuncio',
+        'codigo',
+        'oculto',
+    ];
+}
