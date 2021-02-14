c31f3477da0f8b55468bc3be7a00dd2b71559d55
Angela Conde <56882332+angelaconde@users.noreply.github.com>
2021-02-14 13:48:24 +0100

    Improved the footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,14 @@
 <!-- FOOTER -->
 <div class="container-fluid p-0 pt-5">
     <div class="navbar navbar-dark bg-primary">
-        <div class="container justify-content-center">
-            <p class="navbar-text">© {{ now()->year }} Angela Conde</p>
+        <div class="container justify-content-space-between">
+            <img src="{{ asset('/img/envio_gratis.png') }}" style="width: 200px">
+            <div class="text-center">
+                <a href="" class="navbar-text text-white h4">TÉRMINOS DE USO</a>
+                <br>
+                <p class="navbar-text">© {{ now()->year }} Angela Conde</p>
+            </div>
+            <img src="{{ asset('/img/paypal_footer.jpg') }}" class="rounded" style="width: 319px">
         </div>
     </div>
 </div>
