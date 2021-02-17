5e3fe14ef734b8b84b2dd316b6ae66f180e5392e
Angela Conde <56882332+angelaconde@users.noreply.github.com>
2021-02-17 12:19:30 +0100

    Minor accesibility fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('img/logo.png') }}" alt="">
+                    <img src="{{ asset('img/logo.png') }}" alt="Supermercado La Marisma">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
